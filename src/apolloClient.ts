8020450c3a6084cbfac70248ac3895441f74a528
Berkayergun16 <berkay.ergun16@gmail.com>
2022-05-31 11:19:29 +0300

    init

---
+++ src/apolloClient.ts
@@ -0,0 +1,24 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+// const apolloClient = new ApolloClient({
+//   uri: "http://10.110.247.38:81/graphql",
+//   cache: new InMemoryCache(
+//     {
+//       addTypename:false,
+//     }
+//   ),
+
+// });
+
+
+const apolloClient = new ApolloClient({
+  uri: "http://localhost:5000/graphql/",
+  cache: new InMemoryCache(
+    {
+      addTypename:false,
+    }
+  ),
+
+});
+
+export default apolloClient;
